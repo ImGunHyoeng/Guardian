b3ad26cecad711d4c377e799b13baf9948cf0c5f
ImGunHyoeng <ynoe30@gmail.com>
2024-08-11 01:47:09 +0900

    Add: Solider 공격 및 탐색 이동 AI 구현

--- Source/Caiman/Enemy/Soldier.h
+++ Source/Caiman/Enemy/Soldier.h
@@ -15,4 +15,7 @@ class CAIMAN_API ASoldier : public AGuardianEnemyBase
 	GENERATED_BODY()
 public:
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
